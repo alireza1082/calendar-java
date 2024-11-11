093edca28568ac4e5ea0f84ab8cd9051779705b6
alireza <rezaali1387@gmail.com>
2024-11-11 19:09:58 +0330

    Iranian Calendar

--- Main.java
+++ Main.java
@@ -1,17 +1,93 @@
+import java.util.Calendar;
+import java.util.Locale;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    static Scanner scanner = new Scanner(System.in);
+    static Calendar calendar = new JalaliCalendar();
+    static int year;
+    static int month;
+    static String monthName;
+    static int monthLength;
 
+    public static void main(String[] args) {
+        getInputs();
+
+        printMonth();
+        System.out.print("End");
+    }
+
+    private static void getInputs() {
         System.out.print("Enter full year (e.g., 1401): ");
-        int year = scanner.nextInt();
+        year = scanner.nextInt();
 
         System.out.print("Enter month in number between 1 and 12: ");
-        int month = scanner.nextInt();
-        String a = getMonthName(month);
+        month = scanner.nextInt();
+        monthName = getMonthName(month);
+    }
 
-        System.out.print("\t\t\t--" + year + "  " + a + " \n");
+    private static int calculateFirstDate() {
+        JalaliCalendar.YearMonthDate a =
+                new JalaliCalendar.YearMonthDate(year, month - 1, 0);
+        return JalaliCalendar.dayOfWeek(a);
+    }
+
+    private static String getMonthName(int month) {
+        String a = "";
+        switch (month) {
+            case 1:
+                a = "فروردین";
+                monthLength = 31;
+                break;
+            case 2:
+                a = "اردیبهشت";
+                monthLength = 31;
+                break;
+            case 3:
+                a = "خرداد";
+                monthLength = 31;
+                break;
+            case 4:
+                a = "تیر";
+                monthLength = 31;
+                break;
+            case 5:
+                a = "مرداد";
+                monthLength = 31;
+                break;
+            case 6:
+                a = "شهریور";
+                monthLength = 31;
+                break;
+            case 7:
+                a = "مهر";
+                monthLength = 30;
+                break;
+            case 8:
+                a = "آبان";
+                monthLength = 30;
+                break;
+            case 9:
+                a = "آذر";
+                monthLength = 30;
+                break;
+            case 10:
+                a = "دی";
+                monthLength = 30;
+                break;
+            case 11:
+                a = "بهمن";
+                monthLength = 30;
+                break;
+            case 12:
+                a = "اسفند";
+                monthLength = JalaliCalendar.isLeepYear(year) ? 30 : 29;
+            default:
+        }
+        return a;
+    }
+
+    private static void printMonth() {
         System.out.print(
                 "جمعه" +
                         "\t\tينج‌شنبه" +
@@ -20,50 +96,25 @@ public class Main {
                         "\t\tدوشنبه" +
                         "\t\tبكشنبه" +
                         "\t\tشنبه\n");
-
-        System.out.print("End");
-    }
-
-    private static String getMonthName(int month) {
-        String a = "";
-        switch (month) {
-            case 1:
-                a = "January";
-                break;
-            case 2:
-                a = "February";
-                break;
-            case 3:
-                a = "March";
-                break;
-            case 4:
-                a = "April";
-                break;
-            case 5:
-                a = "May";
-                break;
-            case 6:
-                a = "June";
-                break;
-            case 7:
-                a = "July";
-                break;
-            case 8:
-                a = "August";
-                break;
-            case 9:
-                a = "September";
-                break;
-            case 10:
-                a = "October";
-                break;
-            case 11:
-                a = "November";
-                break;
-            case 12:
-                a = "December";
-            default:
+        int startIndex = 0;
+        for (int i = 0; i < 5; i++) {
+            if (i == 0) startIndex = 7 - calculateFirstDate();
+            else startIndex = 7 * i + calculateFirstDate();
+            System.out.print("   ");
+            for (int j = startIndex; j > startIndex - 7 && j > 0; j--) {
+                if (j > monthLength) System.out.print("\t\t\t\t");
+                else System.out.print(j + "\t\t\t ");
+            }
+            System.out.print("\n");
         }
-        return a;
+    }
+    enum Date {
+        Saturday,
+        Sunday,
+        Monday,
+        Tuesday,
+        Wednesday,
+        Thursday,
+        Friday,
     }
 }
